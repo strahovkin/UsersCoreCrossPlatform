6ba904db2aef54709e9eb477a8545ced25994548
Strahovkin <strahovkin@users.noreply.github.com>
2019-03-10 22:53:23 +0300

    Email and profile picture added

--- Sources/UsersCoreCrossPlatform/User.swift
+++ Sources/UsersCoreCrossPlatform/User.swift
@@ -7,24 +7,28 @@
 
 import Foundation
 
-public final class User: Codable {
-    public var id: UUID?
-    public var name: String
-    public var username: String
-    public var password: String
+final class User: Codable {
+    var id: UUID?
+    var name: String
+    var username: String
+    var password: String
+    var email: String
+    var profilePicture: String?
     
-    public init(name: String, username: String, password: String) {
+    init(name: String, username: String, password: String, email: String, profilePicture: String? = nil) {
         self.name = name
         self.username = username
         self.password = password
+        self.email = email
+        self.profilePicture = profilePicture
     }
     
-    public final class Public: Codable {
-        public var id: UUID?
-        public var name: String
-        public var username: String
+    final class Public: Codable {
+        var id: UUID?
+        var name: String
+        var username: String
         
-        public init(id: UUID?, name: String, username: String) {
+        init(id: UUID?, name: String, username: String) {
             self.id = id
             self.name = name
             self.username = username
